af6fd0dcff00af50fde723e3a41250d8e882e1bd
Elena <e.n.baskaeva@gmail.com>
2020-04-21 16:52:39 +0700

    fix java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,6 @@
 public class CreditPaymentService {
-    public int calculate( int CreditTearm, int amount, double percent) {
-        double CreditPayment = amount * (percent/100/12 + (percent / 100 / 12) / (Math.pow (1 + percent / 100 / 12, CreditTearm) - 1));
-        return (int) CreditPayment; }
+    public int calculate(int creditTearm, int amount, double percent) {
+        double creditPayment = amount * (percent / 100 / 12 + (percent / 100 / 12) / (Math.pow(1 + percent / 100 / 12, creditTearm) - 1));
+        return (int) creditPayment;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        int CreditPaymentOneYear = service.calculate(12, 1_000_000, 9.99 );
+        int CreditPaymentOneYear = service.calculate(12, 1_000_000, 9.99);
         System.out.println(CreditPaymentOneYear);
 
-        int CreditPaymentTwoYears = service.calculate(24, 1_000_000, 9.99 );
+        int CreditPaymentTwoYears = service.calculate(24, 1_000_000, 9.99);
         System.out.println(CreditPaymentTwoYears);
 
-        int CreditPaymentThreeYears = service.calculate(36, 1_000_000, 9.99 );
+        int CreditPaymentThreeYears = service.calculate(36, 1_000_000, 9.99);
         System.out.println(CreditPaymentThreeYears);
     }
 }
